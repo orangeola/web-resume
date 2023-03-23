e51350df2cc55136b99de4608834b24cde55c11a
orangeola <orangeqmm@gmail.com>
2023-03-23 21:11:39 +1030

    Add pinformation for other projects

--- src/components/WorkSection.js
+++ src/components/WorkSection.js
@@ -1,11 +1,14 @@
 import '../styles/WorkSection.css'
 import link_black from '../assets/link_black.svg'
 import github from '../assets/github-mark.svg'
+import youtube from '../assets/youtube.svg'
 
 function WorkSection(props) {
   const work = props.workObj;
   return (
     <div className="workSection" key={work.title}>
+
+      {!work.yt && 
       <div className="workTitle">
         <h2>{work.title}</h2>
         <div>
@@ -13,6 +16,18 @@ function WorkSection(props) {
           <img className="logo" src={link_black} style={{filter: 'invert(99%) sepia(0%) saturate(2%) hue-rotate(291deg) brightness(108%) contrast(100%)'}} onClick={()=> window.open(work.live)} title="live website"/>
         </div>
       </div>
+      }
+
+      {work.yt && 
+      <div className="workTitle">
+        <h2>{work.title}</h2>
+        <div>
+          <img className="logo" src={youtube} style={{filter: 'invert(99%) sepia(0%) saturate(2%) hue-rotate(291deg) brightness(108%) contrast(100%)'}} onClick={()=> window.open(work.link)} title="youtube link"/>
+        </div>
+      </div>
+      }
+
+
       <img className="demoPicture" src={work.img}/>
       <p>{work.dsc}</p>
     </div>
--- src/components/WorkTab.js
+++ src/components/WorkTab.js
@@ -1,47 +1,53 @@
 import '../styles/WorkTab.css'
 import tictactoe from '../assets/tictactoe.png'
+import odblogweb from '../assets/odblogweb.png'
+import salespree from '../assets/salesspree.png'
+import membersonly from '../assets/membersonly.png'
+import whereswally from '../assets/wheresod.png'
+import todolist from '../assets/todolist.png'
+
 import WorkSection from '../components/WorkSection'
 
 function WorkTab() {
   const ttt = [{
-    title: "Tic Tac Toe",
-    img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
-    git: "https://github.com/orangeola/odin-tictactoe",
-    live: "https://orangeola.github.io/odin-tictactoe/"
+    title: "Blog",
+    img: odblogweb,
+    dsc: "A RESTful API Express server that serves two different React front-ends. One is for general users who can view posts and post a comment. The other is a website just for me to make new posts, or delete posts and comments. Utilises a JWT token to verify whether the admin has logged in to make these changes.",
+    git: "https://github.com/orangeola/od-blog-web",
+    live: "https://orangeola.github.io/od-blog-web/"
+  },
+  {
+    title: "Sale Spree",
+    img: salespree,
+    dsc: "A couch co-op party game made in six months with a team of six using Unity and C#. Race against the clock as you try to fulfill your randomised shopping list while battling other shoppers along the way.",
+    link: "https://youtu.be/gTXo4zMmHLU",
+    yt: true
+  },
+  {
+    title: "Where's Wally",
+    img: whereswally,
+    dsc: "Find three things hidden in this Where's Wally inspired picture. Every click calls Firebase to check if one of the objectives have been found. Made using React.",
+    git: "https://github.com/orangeola/wheres-od",
+    live: "https://orangeola.github.io/wheres-od/"
+  },
+  {
+    title: "Members Only",
+    img: membersonly,
+    dsc: "Any registered user may make a post here, but only members can see the name of the author. If you know the secret password, you can become a memeber... or even an admin. Utilises passpotjs to handle log in and uses bcryptjs to encrypt passwords in the database. Hosted entirely within Express using pug.",
+    git: "https://github.com/orangeola/od-members-only",
+    live: "https://od-members-only.orangeola.repl.co"
+  },
+  {
+    title: "To-do List",
+    img: todolist,
+    dsc: "Add to-do's within topics to keep them grouped and ordered. Every topic and to-do is stored on a mongodb database.",
+    git: "https://github.com/orangeola/od-todo-list",
+    live: "https://orangeola.github.io/od-todo-list/"
   },
   {
     title: "Tic Tac Toe",
     img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
-    git: "https://github.com/orangeola/odin-tictactoe",
-    live: "https://orangeola.github.io/odin-tictactoe/"
-  },
-  {
-    title: "Tic Tac Toe",
-    img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
-    git: "https://github.com/orangeola/odin-tictactoe",
-    live: "https://orangeola.github.io/odin-tictactoe/"
-  },
-  {
-    title: "Tic Tac Toe",
-    img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
-    git: "https://github.com/orangeola/odin-tictactoe",
-    live: "https://orangeola.github.io/odin-tictactoe/"
-  },
-  {
-    title: "Tic Tac Toe",
-    img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
-    git: "https://github.com/orangeola/odin-tictactoe",
-    live: "https://orangeola.github.io/odin-tictactoe/"
-  },
-  {
-    title: "Tic Tac Toe",
-    img: tictactoe,
-    dsc: "Tic Tac Toe in the style of Windows XP. Enemy AI makes decision using the minimax algorithm.",
+    dsc: "Tic Tac Toe in the style of the Windows XP era. Enemy AI makes decision using the minimax algorithm.",
     git: 'https://github.com/orangeola/odin-tictactoe',
     live: 'https://orangeola.github.io/odin-tictactoe'
   },]
