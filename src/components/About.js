a4def634c55f6835145c9af3ab4c9ff108444c9a
orangeola <orangeqmm@gmail.com>
2023-04-02 22:47:01 +0930

    Change about me section

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,13 @@ function About() {
       <div className="AboutText">
         <h1>About <span style={{color: 'red'}}>me.</span></h1>
         <p>
-          Hi, I'm Alexander. After completing my computer science degree, I decided that it was the perfect time to continue studying and explore other potential careers within software development. I promptly studied game development for a year, and then full-stack web development. 
+          Hi, I'm Alexander. After completing my computer science degree, I decided to continue studying other areas of software development before committing to a career. I promptly studied game development for a year, and then full-stack web development.
         </p>
         <p>
-          After all these years of study, I feel ready and committed to contribute to meaningful projects with others. As an avid life-long learner, I am excited to begin the next stage of my life and develop myself both professionally and personally while I help build projects greater than myself.
+          After these years of study, I feel ready and committed to contribute to meaningful projects with others. As an avid life-long learner, I am excited to begin the next stage of my life and develop myself both professionally and personally while I help build projects greater than myself.
         </p>
         <p>
-          Outside coding, I enjoy reading (with the occasional bit of writing) along with trips to the cinema. I also enjoy playing badminton and tennis, as well as going on hikes out in nature. I have also recently picked up a guitar, and am excited to learn!
+          Outside coding, I enjoy reading (with the occasional bit of writing) along with trips to the cinema. I enjoy playing badminton and tennis, as well as going on hikes out in nature. I have also recently picked up a guitar, and am excited to learn!
         </p>
       </div>
     </div>
