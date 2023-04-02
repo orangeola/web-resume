7d265248c2afdd19d05864fec4a48a7e2b3312f3
orangeola <orangeqmm@gmail.com>
2023-04-02 22:54:50 +0930

    Add typed.js to title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import '../styles/Home.css'
+import React from 'react';
+import Typed from 'typed.js';
 
 function Home() {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Alexander'],
+      typeSpeed: 140,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+
   return (
     <div className="Home">
-        <h1>Alexander</h1>
+        <h1><span ref={el} /></h1>
     </div>
   );
 }
